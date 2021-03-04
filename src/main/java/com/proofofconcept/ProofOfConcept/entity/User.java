f9086ad997d3f52df601b34ee0a05a9527cc6ba5
shravan20 <mrshravankumarb@gmail.com>
2021-03-04 18:44:20 +0530

    :sparkles: Added JAVAx packages|Updated EntityClass|Add Annotations

--- src/main/java/com/proofofconcept/ProofOfConcept/entity/User.java
+++ src/main/java/com/proofofconcept/ProofOfConcept/entity/User.java
@@ -3,10 +3,19 @@ package com.proofofconcept.ProofOfConcept.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
-
+@Entity
+@Table(name = "UserInfo")
 public class User {
+    /**
+     * User Entity
+     */
     private String username;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 }
