8941d27fbd69169412687f226aabee34fc2423d2
shravan20 <mrshravankumarb@gmail.com>
2021-03-04 12:47:50 +0530

    :sparkles: Basic GET API

---
+++ src/main/java/com/proofofconcept/ProofOfConcept/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.proofofconcept.ProofOfConcept.controller;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity getUser(@PathVariable String userId) {
+        String user = "Shravan Kumar B";
+        HashMap <String, String> userData = new HashMap<>();
+        userData.put("name", user);
+        userData.put("id", userId);
+
+        return new ResponseEntity<>(userData, HttpStatus.OK);
+    }
+}
