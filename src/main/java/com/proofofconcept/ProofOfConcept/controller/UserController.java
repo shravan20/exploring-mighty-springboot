5c421053c89f486cc95ccfb0ef963ecbfb1a5773
shravan20 <mrshravankumarb@gmail.com>
2021-03-04 19:06:47 +0530

    :sparkles: Autowired Controller with service

--- src/main/java/com/proofofconcept/ProofOfConcept/controller/UserController.java
+++ src/main/java/com/proofofconcept/ProofOfConcept/controller/UserController.java
@@ -2,6 +2,8 @@ package com.proofofconcept.ProofOfConcept.controller;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.proofofconcept.ProofOfConcept.entity.User;
+import com.proofofconcept.ProofOfConcept.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,18 @@ import java.util.HashMap;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+
     @GetMapping(value = "/{userId}")
-    public ResponseEntity getUser(@PathVariable String userId) {
-        User userObject = new User();
-        userObject.setUsername("Shravan Kumar B");
-        userObject.setUserId(213);
-        return new ResponseEntity<>(userObject, HttpStatus.OK);
+    public ResponseEntity getUser(@PathVariable int userId) {
+        return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity createUser(@RequestBody User userDetails){
-        return new ResponseEntity<>(userDetails, HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.addUser(userDetails), HttpStatus.CREATED);
     }
 
 
