e2fbccacce9f5e9558d9e2b80b57e71bad2004d2
shravan20 <mrshravankumarb@gmail.com>
2021-03-04 12:12:22 +0530

    :tada: Initialised Structure Commit

---
+++ src/main/java/com/proofofconcept/ProofOfConcept/ProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package com.proofofconcept.ProofOfConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofOfConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proofofconcept/ProofOfConcept/ProofOfConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proofofconcept.ProofOfConcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProofOfConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
