e98d0a3b4ed4f318176430ffbed21cf6a10c9651
shravan20 <mrshravankumarb@gmail.com>
2021-03-26 14:26:40 +0530

    Minor Updates

--- src/main/java/com/proofofconcept/ProofOfConcept/entity/User.java
+++ src/main/java/com/proofofconcept/ProofOfConcept/entity/User.java
@@ -2,6 +2,7 @@ package com.proofofconcept.ProofOfConcept.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Setter
 @Entity
 @Table(name = "UserInfo")
+@ToString
 public class User {
     /**
      * User Entity
